f5396adbda78a952f27b8130f2a1221ba35c25e1
agthalele <agatha.leticia.327@gmail.com>
2024-08-22 10:15:55 -0300

    OKOKOK

--- Main.java
+++ Main.java
@@ -1,45 +1,45 @@
 import java.util.ArrayList;
-import java.util.Scanner;
+//import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
 
-        Scanner teclado = new Scanner(System.in);
+        /*Scanner teclado = new Scanner(System.in);
         
         System.out.println("Nome do Banco: ");
-        String nomebanco = teclado.nextLine( );
+        String nomebanco = teclado.nextLine( );*/
 
-        Banco banco = new Banco(nomebanco);
+        Banco banco = new Banco("nomebanco");
 
 
-        System.out.println("Nome: ");
+        /*System.out.println("Nome: ");
         String nome = teclado.nextLine( );
 
 
         System.out.println("Cpf: ");
-        String cpf = teclado.nextLine( );  
+        String cpf = teclado.nextLine( );  */
 
 
-        ClientePF clientepf = new ClientePF(nome, cpf);
+        ClientePF clientepf = new ClientePF("nome", "cpf");
         banco.adicionaCliente(clientepf);
 
 
         //fariamos a mesma coisa para juridica
-        System.out.println("Empresa: ");
+       /*  System.out.println("Empresa: ");
         String empresa = teclado.nextLine( );
 
 
         System.out.println("Cnpj: ");
-        String cnpj = teclado.nextLine( );
+        String cnpj = teclado.nextLine( ); */
 
 
-        ClientePJ clientecnpj = new ClientePJ(empresa, cnpj);
+        ClientePJ clientecnpj = new ClientePJ("empresa", "cnpj");
         banco.adicionaCliente(clientecnpj);
 
 
        ArrayList<Cliente> listaDeClientes = banco.getClientes();
 
-        System.out.println("\n\t"+nomebanco);
+        System.out.println("\n\tnomebanco");
        for(int i = 0; i< listaDeClientes.size(); i++){
  
         System.out.println("-------------------------------");
@@ -61,7 +61,7 @@ public class Main {
 
 
 
-       teclado.close();
+       //teclado.close();
 
 
     }
